a1987d601bea8557572cbd1d496f6cf1aa05b7fa
Obasi Ikechukwu Thompson <obasiikechukwu761@gmail.com>
2022-10-25 13:00:24 +0100

    first push

---
+++ script.js
@@ -0,0 +1,31 @@
+
+console.log(document.getElementById("rate").value)
+function compute()
+{    
+    var principal =document.getElementById("principal").value; 
+    var rate=document.getElementById("rate").value;
+    var years=document.getElementById("years").value;
+    var interest=principal*years*rate/100;
+    var year=new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit " +principal+ " at an interest rate of "+rate+ ", you will receive an amount of "+interest+ " in the year "+year
+    
+}
+    //function for the range slider
+    function updateRate(){
+        var rateval=document.getElementById("rate").value;
+        document.getElementById("rate_val").innerText=rateval;
+    }
+
+function validate(){
+    var check=document.getElementById("principal").value;
+
+    if(check <=0){
+        alert("enter a positive number");
+        document.getElementById("principal").focus()
+        return false
+        }
+    else{
+        alert("good")
+        return true
+        }
+}  
